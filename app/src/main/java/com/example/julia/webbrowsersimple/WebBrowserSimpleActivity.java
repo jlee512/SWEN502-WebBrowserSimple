4896e476941fabf6415e349f2c823e4520b6db5f
Julian Lees <julian.j.lees@gmail.com>
2017-10-19 01:16:05 +1300

    Basic front page view created with search interface interaction - cursor added after initial text is typed

--- app/src/main/java/com/example/julia/webbrowsersimple/WebBrowserSimpleActivity.java
+++ app/src/main/java/com/example/julia/webbrowsersimple/WebBrowserSimpleActivity.java
@@ -1,7 +1,14 @@
 package com.example.julia.webbrowsersimple;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class WebBrowserSimpleActivity extends AppCompatActivity {
 
@@ -9,5 +16,39 @@ public class WebBrowserSimpleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_browser_simple);
+
+        EditText text = (EditText) findViewById(R.id.editText);
+        text.setHintTextColor(Color.parseColor("#057eaa"));
+
+        //Text onclick listener to detect if text is selected
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = (EditText) findViewById(R.id.editText);
+                text.setHintTextColor(getResources().getColor(R.color.orangeFontColor));
+                text.setTextColor(getResources().getColor(R.color.orangeFontColor));
+                if (text.getText().length() > 0) {
+                    text.setCursorVisible(true);
+                } else {
+                    text.setCursorVisible(false);
+                }
+            }
+        });
+
+        View overallLayout = (View) findViewById(R.id.overallLayout);
+
+        //Overall layout click listener to detect if anything other than text is selected
+        overallLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = (EditText) findViewById(R.id.editText);
+                text.setHintTextColor(getResources().getColor(R.color.blueFontColor));
+                text.setTextColor(getResources().getColor(R.color.blueFontColor));
+                text.setCursorVisible(false);
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+
     }
 }
